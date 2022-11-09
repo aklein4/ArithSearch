737763a2fca879686d1c5c5f9d7761f9099e122e
Adam Klein <adam3207@gmail.com>
2022-11-09 14:55:46 -0800

    MCMC runs but has circular dependency issue

--- stochastic.py
+++ stochastic.py
@@ -1,69 +1,180 @@
 
 import numpy as np
-import numpy.polynomial as poly
+import random
+import itertools
+import sys
+import time
 from sklearn.metrics import mean_squared_error
 
 from super_circuit import SuperCircuit
+from circuit import OPERATIONS
 
 """
 Framework for potential stochastic search algorithm.
 """
 
-def poly_MSE(a_poly, b_poly):
-    """
-    Mean squared error between the coefficients of poly.Polynomials.
-    If one polynomial is lower degree, its coefficients are zero-padded.
-    """
+def random_op():
+    return OPERATIONS.ADD if random.random() >= 0.5 else OPERATIONS.MULT
 
-    a_size = a_poly.degree() + 1
-    b_size = b_poly.degree() + 1
-    max_n = max(a_size, b_size)
-
-    a, b = np.zeros(max_n), np.zeros(max_n)
-    a[:a_size] = a_poly.coef
-    b[:b_size] = b_poly.coef
-
-    return mean_squared_error(a, b)
+def not_my_op(me):
+    if me.operation is OPERATIONS.ADD:
+        return OPERATIONS.MULT
+    return OPERATIONS.ADD
 
 class SearchEngine:
 
-    def __init__(self, circuit: SuperCircuit):
-        self.circuit = circuit.copy()
+    def __init__(self, target_func, n_args, costs={OPERATIONS.MULT: 1, OPERATIONS.ADD: 1}, n_vals=10):
+        self.circuit = SuperCircuit(n_args, costs=costs)
+        self.target_func = target_func
+        self.n_args = n_args
 
-    def search(self, max_iters):
+        for i in range(1, n_vals+1):
+            self.circuit.getValNode(i)
+            self.circuit.getValNode(-i)
+
+    def get_samples(self, samples_per_arg):
+        size = samples_per_arg**self.n_args
+        samples = np.zeros((self.n_args, size))
+
+        seen = set()
+        iters = itertools.combinations_with_replacement([i for i in range(samples_per_arg)], self.n_args)
+        ind = 0
+        for it in iters:
+            perms = itertools.permutations(it)
+            for perm in perms:
+                if perm not in seen:
+                    for arg in range(self.n_args):
+                        samples[arg][ind] = perm[arg]
+                    ind += 1
+                    seen.add(perm)
+
+        if len(samples.shape) == 1:
+            samples = np.expand_dims(samples, 0)
+        return samples.astype(np.int64)
+
+    def get_target_output(self, samples):
+        return self.target_func(samples)
+
+    def search(self, max_iters, samples_per_arg, C_cost, C_acc, p_accept, clean_freq=100, verbose=True):
+
+        # things relating to the target
+        samples = self.get_samples(samples_per_arg)
+        target = self.get_target_output(samples)
 
         # keep track of the best solution that we have found
         best_solution = None
+        old_score = None
+
+        # actions to choose from
+        actions = [
+            i for i in range(6)
+        ]
+
+        # verbose stuff
+        last_time = time.time_ns()
+
+        found_solutions = 0
+        accepted_changes = 0
+        rejected_changes = 0
+        failed_changes = 0
 
         # do iterations
         for iter in range(max_iters):
 
+            if verbose:
+                new_time = time.time_ns()
+                if iter == 0 or iter == max_iters-1 or (new_time-last_time)*1e-9 >= 0.25:
+                    last_time = new_time
+                    new_msg = "iteration: " + str(iter) + "/" + str(max_iters)
+                    new_msg += "\nbest solution: " + ("None" if best_solution == None else str(best_solution.cost))
+                    new_msg += "\nfound solutions: " + str(found_solutions)
+                    new_msg += "\naccepted changes: " + str(accepted_changes)
+                    new_msg += "\nrejected changes: " + str(rejected_changes)
+                    new_msg += "\nfailed changes: " + str(failed_changes)
+                    new_msg += "\ncurr cost: " + str(self.circuit.cost) + "\n\n"
+                    sys.stdout.write(new_msg)
+                    sys.stdout.flush()
+                    msg = new_msg
+
             # try making a change to the node
-            change = create_some_change
-            valid = circuit.do_change(change) # if False change was invalid and not done
+            change = 0
+            if len(self.circuit.nodes) > 0:
+                change = random.sample(actions, 1)[0]
+            old_cost = self.circuit.cost
+            valid = True
+
+            if change == 0:
+                # new root
+                self.circuit.addNode(random_op(), [
+                    self.circuit.root if not self.circuit.root is None else random.sample(self.circuit.all_nodes, 1)[0],
+                    random.sample(self.circuit.all_nodes, 1)[0]
+                ])
+
+            elif change == 1:
+                # change op
+                n = random.sample(self.circuit.nodes, 1)[0]
+                valid = self.circuit.change_operation(n, not_my_op(n))
+
+            elif change == 2:
+                # insert randomly
+                upper = random.sample(self.circuit.nodes, 1)[0]
+                base = random.sample(upper.operands, 1)[0]
+                other = random.sample(set(self.circuit.arg_leaves) | set(self.circuit.val_leaves.values()), 1)[0]
+                new_node = self.circuit.addNode(random_op(), [base, other])
+                valid = self.circuit.change_input(upper, base, new_node)
+
+            elif change == 3:
+                base = random.sample(self.circuit.nodes, 1)[0]
+                old_op = random.sample(base.operands, 1)[0]
+                new_op = random.sample(self.circuit.all_nodes, 1)[0]
+                valid = self.circuit.change_input(base, old_op, new_op)
+
+            elif change >= 4:
+                # remove rand
+                node = random.sample(self.circuit.nodes, 1)[0]
+                replace = random.sample(node.operands, 1)[0]
+                valid = self.circuit.removeNode(node, replace)
+
+            self.circuit.recalc_cost()
 
             # make sure that this change is valid
             if valid:
 
-                # check whether we want to keep this change, according to the metaheuristic
-                accept = flip_accept_coin(self.circuit.cost)
-                if accept:
+                # try this circuit on the samples
+                output = self.circuit.evaluate(samples)
 
-                    # check is we have found a new best solution
-                    if best_solution == None or (self.circuit.is_solution and self.circuit.cost < best_solution.cost):
+                if np.array_equal(output, target):
+                    # this is a solution
+                    found_solutions += 1
+                    if best_solution is None or self.circuit.cost < best_solution.cost:
                         best_solution = self.circuit.copy()
-                
+
+                # get new score from change
+                new_score = C_cost*self.circuit.cost + C_acc*mean_squared_error(target, output)
+
+                if np.array_equal(output, target) or old_score == None or new_score <= old_score or random.random() <= p_accept:
+                    # take this new circuit
+                    old_score = new_score
+                    accepted_changes += 1
                 else:
-
                     # undo the last change because it was bad
-                    self.circuit.undo_change(change)
+                    self.circuit.undo()
+                    self.circuit.cost = old_cost
+                    rejected_changes += 1
+            
+            else:
+                failed_changes += 1
 
+            # clean memory
+            if iter % clean_freq == 0:
+                self.circuit.clean()
+
+        self.circuit.clean()
         return best_solution
 
 def main():
-    a = poly.Polynomial([1, 2])
-    b = poly.Polynomial([1, 3])
-    print(poly_MSE(a, b))
+    engine = SearchEngine(lambda x: 3*x[0]**4 + 4*x[0]**2 + 2*x[0] + 1, 1, n_vals=2, costs={OPERATIONS.MULT: 1, OPERATIONS.ADD: .25})
+    engine.search(100000, 10, 1, 1, 0.5)
 
 if __name__ == '__main__':
     main()
--- super_circuit.py
+++ super_circuit.py
@@ -1,11 +1,10 @@
 
 from circuit import OPERATIONS, OpInfo
+import numpy as np
 
 class SuperNode:
 
     def __init__(self, operands=None, operation=None, is_leaf=False, arg=None, val=None):
-        if operands != None and type(operands) is not set:
-            raise ValueError("Operands must be presented as a set!")
 
         # make sure args are coherant
         if not is_leaf and (operands == None or operation == None):
@@ -43,7 +42,7 @@ class SuperNode:
 
     def remove_output(self, out):
         # remove node as output of self
-        self.outputs.remove(out)
+        self.outputs.discard(out)
 
 
     def change_input(self, old_input, new_input):
@@ -57,15 +56,16 @@ class SuperNode:
         old_input.remove_output(self)
         new_input.set_output(self)
 
-        self.operands.remove(old_input)
-        self.operands.add(new_input)
+        self.operands[self.operands.index(old_input)] = new_input
 
         self._update_depends()
+        if self in self.depends_on:
+            raise RuntimeError("Node is dependent on itself")
         return True
         
     def _update_depends(self):
 
-        new_depends = self.operands.copy()
+        new_depends = set(self.operands)
         for op in self.operands:
             new_depends = new_depends | op.depends_on
 
@@ -83,6 +83,11 @@ class SuperCircuit:
         # all nodes that are not leaves
         self.nodes = set()
 
+        # all nodes including leaves
+        self.all_nodes = set()
+
+        self._unclean = set()
+
         # hold const leaves in dict with val -> node
         self.val_leaves = {}
 
@@ -90,14 +95,8 @@ class SuperCircuit:
         self.arg_leaves = [
             SuperNode(is_leaf=True, arg=i) for i in range(n_args)
         ]
-
-        # get root node of tree
-        self.root = None
-
-        # keep track of the number of circle nodes that depend on a given one
-        self.num_dep = {}
-        # keep track of which cicle nodes have multiple outputs
-        self.multi_out = set()
+        for a in self.arg_leaves:
+            self.all_nodes.add(a)
 
         # keep track of how mush operations cost
         self.op_costs = costs.copy()
@@ -105,8 +104,10 @@ class SuperCircuit:
         self.cost = 0
 
         # the action that undoes the last action
-        self.undo_action = None
+        self.undo_stack = []
 
+        # get root node of tree
+        self.root = None
 
     def getValNode(self, val):
         """
@@ -117,6 +118,8 @@ class SuperCircuit:
         else:
             new_node = SuperNode(is_leaf=True, val=val)
             self.val_leaves[val] = new_node
+            self.all_nodes.add(new_node)
+            self._unclean.add(new_node)
             return new_node
 
 
@@ -134,33 +137,70 @@ class SuperCircuit:
         return self.root
 
 
-    def addNode(self, operation, operands):
+    def _addNode_wrap(self, args):
+        self.addNode(None, None, args[0])
+        for i in range(args[1]):
+            self.undo()
+    def addNode(self, operation, operands, node=None):
         """
         Add a new node to the tree
         :arg operation: the operation that this node performs
         :arg operands: Iterable of operands that this will take as inputs
         """
-        # TODO: Check if this violates circular dependency
 
         # create the new node
-        new_node = SuperNode(operation=operation, operands=operands)
+        new_node = node
+        if node == None:
+            new_node = SuperNode(operation=operation, operands=operands)
+        else:
+            operation = node.operation
+            operands = node.operands
         # add it to the register
         self.nodes.add(new_node)
-        self.num_dep[new_node] = 0
+        self.all_nodes.add(new_node)
+        self._unclean.add(new_node)
         self.cost += self.op_costs[operation]
 
-        # add this dependency to its child nodes
-        for op in operands:
-            if not op.is_leaf:
-                self.num_dep[op] += 1
-                if self.num_dep[op] > 1:
-                    self.multi_out.add(op)
-
         # this is the new root node if it depends on the old root
+        old_root = None
         for op in operands:
             if self.root == None or op is self.root:
+                old_root = op
                 self.root = new_node
                 break
+        if old_root == None:
+            for op in operands:
+                old_root = op
+                break
+        
+        self.undo_stack.append({"func": self._removeNode_wrap, "args": (new_node, old_root)})
+        return new_node
+
+    def _removeNode_wrap(self, args):
+        self.removeNode(args[0], args[1])
+    def removeNode(self, node, keeper):
+        if not keeper in node.operands:
+            raise RuntimeError("Tried removing node, but keeper is not operand")
+
+        num_changes = len(node.outputs)
+        coped = node.outputs.copy()
+        for out in coped:
+            succ = self.change_input(out, node, keeper)
+            if not succ:
+                raise RuntimeError("fuck, input change failed in removeNode (this shouldn't happen)")
+
+        self.cost -= self.op_costs[node.operation]
+        self.nodes.discard(node)
+        self.all_nodes.discard(node)
+
+
+        if node is self.root:
+            self.root = keeper
+
+        self.undo_stack.append({"func": self._addNode_wrap, "args": (node, num_changes)})
+        
+        return True
+
 
     def _change_operation_wrap(self, args):
         return self.change_operation(args[0], args[1])
@@ -171,7 +211,9 @@ class SuperCircuit:
         if node.operation == operation:
             return False
 
-        self.undo_action = {"func": self._change_operation_wrap, "args": (node, node.operation)}
+        self.cost -= self.op_costs[node.operation]
+        self.cost += self.op_costs[operation]
+        self.undo_stack.append({"func": self._change_operation_wrap, "args": (node, node.operation)})
         node.operation = operation
         return True
 
@@ -182,64 +224,73 @@ class SuperCircuit:
         succ = node.change_input(old_input, new_input)
 
         if succ:
-            self.undo_action = {"func": self._change_input_wrap, "args": (node, new_input, old_input)}
-
-            if not old_input.is_leaf:
-
-                self.num_dep[old_input] -= 1
-                if self.num_dep[old_input] == 1:
-                    self.multi_out.remove(old_input)
-
-            if not new_input.is_leaf:
-                self.num_dep[new_input] += 1
-                if self.num_dep[new_input] > 1:
-                    self.multi_out.add(new_input)
+            self.undo_stack.append({"func": self._change_input_wrap, "args": (node, new_input, old_input)})
 
         return succ
 
-
     def undo(self):
-        if self.undo_action == None:
-            return
-        
-        self.undo_action["func"](self.undo_action["args"])
+        if self.undo_stack == []:
+            print("WARNING: undo called for circuit with empty undo stack")
+        old_len = len(self.undo_stack)
 
-        self.undo_action == None
+        undo_action = self.undo_stack.pop()
+
+        succ = undo_action["func"](undo_action["args"])
+        if succ is False:
+            print("WARNINGundo failed in circuit")
+        
+        while len(self.undo_stack) > old_len-1:
+            if len(self.undo_stack) == 0:
+                break
+            self.undo_stack.pop()
 
 
     def clean(self):
+        self.undo_stack = []
+
         to_remove = set()
-        for n in self.num_dep.keys():
-            if self.num_dep[n] == 0:
+        for n in self._unclean:
+            if n not in self.root.depends_on and not n is self.root and not n.is_leaf:
                 to_remove.add(n)
-        
+
         for n in to_remove:
-            self.nodes.remove(n)
-            self.num_dep.pop(n)
+            self.nodes.discard(n)
+            self.all_nodes.discard(n)
+            self._unclean.discard(n)
         
         return len(to_remove)
+
+
+    def recalc_cost(self):
+        self.cost = 0
+        for n in self.root.depends_on:
+            if not n.is_leaf:
+                self.cost += self.op_costs[n.operation]
+        if not self.root.is_leaf:
+            self.cost += self.op_costs[self.root.operation]
 
 
     def evaluate(self, args):
 
         outs = {}
-        deps = {}
         track_stack = []
         curr = self.root
 
-        max_stack_size = 1.5*(len(self.nodes) + len(self.arg_leaves) + len(self.val_leaves))
+        max_stack_size = 1.5*(len(self.all_nodes))
         while True:
-            if len(track_stack) > max_stack_size:
-                raise RuntimeError("Evaluation stack depth exceeds limit, circuit likely contains circular dependency.")
+            if curr in track_stack:
+                raise RuntimeError("Circular dependency in evaluate!")
 
             if curr.is_leaf:
                 if curr.arg != None:
+                    if curr is self.root:
+                        return args[curr.arg]
                     outs[curr] = args[curr.arg]
-                    deps[curr] = len(curr.outputs)
                     curr = track_stack.pop()
                 else:
-                    outs[curr] = curr.val
-                    deps[curr] = len(curr.outputs)
+                    if curr is self.root:
+                        return np.full_like(args[0], curr.val)
+                    outs[curr] = np.full_like(args[0], curr.val)
                     curr = track_stack.pop()
 
             else:
@@ -251,27 +302,50 @@ class SuperCircuit:
                 else:
                     if curr not in outs.keys():
 
-                        op_list = list(curr.operands)
-                        if len(op_list) != 2:
-                            raise ValueError("Node does not have two operands!")
+                        if len(curr.operands) != 2:
+                            raise ValueError("Node does not have two operands! " + str(len(curr.operands)))
 
                         outs[curr] = curr.operation.func(
-                            outs[op_list[0]],
-                            outs[op_list[1]]
+                            outs[curr.operands[0]],
+                            outs[curr.operands[1]]
                         )
                         if curr is self.root:
                             break
 
-                        deps[curr] = len(curr.outputs)
-                        for op in curr.operands:
-                            deps[op] -= 1
-                            if deps[op] == 0:
-                                deps.pop(op)
-                                outs.pop(op)
-
                     curr = track_stack.pop()
 
         return outs[self.root]
+
+
+    def copy(self):
+        new_circ = SuperCircuit(self.n_args, costs=self.op_costs)
+
+        other_lib = {}
+        for i in range(len(self.arg_leaves)):
+            other_lib[self.arg_leaves[i]] = new_circ.getArgNode(i)
+        for i in self.val_leaves.keys():
+            other_lib[self.val_leaves[i]] = new_circ.getValNode(i)
+        
+        if self.root.is_leaf:
+            return new_circ
+
+        track_stack = []
+        curr = self.root
+        while True:
+            good = True
+            for op in curr.operands:
+                if not op in other_lib.keys():
+                    track_stack.append(curr)
+                    curr = op
+                    good = False
+                    continue
+            if not good:
+                continue
+            other_ops = [other_lib[op] for op in curr.operands]
+            other_lib[curr] = new_circ.addNode(curr.operation, other_ops)
+            if curr == self.root:
+                return new_circ
+            curr = track_stack.pop()
 
 
     def getSubCircuit(self):
@@ -300,12 +374,15 @@ def main():
     one = s.getValNode(1)
     two = s.getValNode(2)
 
-    s.addNode(OPERATIONS.MULT, set([x, one]))
+    s.addNode(OPERATIONS.MULT, set([x, two]))
     print(s.evaluate([1]))
-    print(s.change_operation(s.root, OPERATIONS.ADD))
+    s.change_operation(s.root, OPERATIONS.ADD)
     print(s.evaluate([1]))
     s.undo()
     print(s.evaluate([1]))
+
+    s_2 = s.copy()
+    print(s_2.evaluate([1]))
 
 if __name__ == '__main__':
     main()
