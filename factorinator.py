be691b6e61d8456dce717db5d6dd081c1c5c2f49
Adam Klein <adam3207@gmail.com>
2022-11-15 10:23:07 -0800

    converted keys to np arrays and other improvements

--- factorinator.py
+++ factorinator.py
@@ -8,6 +8,13 @@ import numpy as np
 import random
 
 
+class Nom:
+    def __init__(self, a: np.ndarray):
+        self.a = a
+    def __hash__(self):
+        return hash(str(self.a))
+
+
 COST_SAVE_ITERS = 10
 
 
@@ -37,97 +44,129 @@ def make_clean(poly: SparsePoly, key_lib=[(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0
     return final, common_d, outliers
 
 def log_cost(k):
-    return sum([np.ceil(np.log2(float(1+abs(k[i])))) for i in range(len(k))])
+    return np.sum(np.ceil(np.log2(1+np.abs(k))))
 
 
 def generalized_horners(poly, verbose:bool=False, mem=None, sampling=None, early_stopping=None):
     if mem is None:
         mem = set()
 
-    group = poly
+    group = set()
     if isinstance(poly, SparsePoly):
-        group = set(poly.dict.keys())
-
-    def get_clean(s: set, meem=mem):
-        clean = set()
-        for k in s:
-            if sum(k) > 1 and k not in meem:
-                clean.add(k)
-        return clean
-
-    def get_reduced(s: set, d: tuple):
-        new_group = set()
-        for k in s:
-            l = tuple([k[_] - d[_] for _ in range(len(k))])
-            if min(l) < 0:
-                raise RuntimeError("Negative exponent in reduction.")
-            new_group.add(l)
-
-        return new_group
-
-    group = get_clean(group)
+        for k in poly.dict.keys():
+            a = np.array(k)
+            group.add(Nom(a))
+    else:
+        for k in poly:
+            group.add(k)
 
     if len(group) == 0:
         return 0
 
-    k_list = list(group)
-    k_list.sort(reverse=True, key=lambda k: sum(k))
+    n = list(group)[0].a.size
 
-    commons = list()
-    for k in k_list:
+    def get_clean(s: set, meem=mem):
+        clean = set()
+        ms = 0
+        for k in s:
+            if np.sum(k.a) > 1 and k not in meem:
+                clean.add(k)
+            else:
+                ms += 2
+        return clean, ms
 
-        found = False
-        for g in range(len(commons)):
-            new_d = tuple([min(commons[g][_], k[_]) for _ in range(len(k))])
-            if sum(new_d) > 0:
-                commons[g] = new_d
-                found = True
-                break
+    def get_reduced(s: set, d: np.ndarray):
+        new_group = set()
+        for k in s:
+            l = k.a - d
+            if min(l) < 0:
+                raise RuntimeError("Negative exponent in reduction.")
+            new_group.add(Nom(l))
 
-        if not found:
-            commons.append(k)
+        return new_group
 
-    if not sampling is None:
-    #     # for i in range(len(k_list[0])):
-    #     #     check = [0 for _ in range(len(k_list[0]))]
-    #     #     check[i] = 1
-    #     #     check = tuple(check)
-    #     #     if check not in commons:
-    #     #         commons.append(check)
-        commons += list(mem)
+    group, cost = get_clean(group)
 
+    if len(group) == 0:
+        return max(cost - 1, 0) # fencepost of addition
+
+    # k_list = list(group)
+    # k_list.sort(reverse=True, key=lambda k: np.sum(k))
+
+
+    # commons = []
+    # for k in k_list:
+
+    #     found = False
+    #     for g in range(len(commons)):
+    #         new_d = tuple([min(commons[g][_], k[_]) for _ in range(len(k))])
+    #         if sum(new_d) > 0:
+    #             commons[g] = new_d
+    #             found = True
+    #             break
+
+    #     if not found:
+    #         commons.append(k)
+
+    commons = []
+    for i in range(n):
+        check = np.zeros(n)
+        check[i] = 1
+        commons.append(check)
+    
+    commons += [m.a for m in mem]
 
     groups = [set() for _ in range(len(commons))]
-    for k in k_list:
-        best = None
-        best_score = None
-        sample_inds = []
-        sample_weights = []
+    while len(group) > 0:
+        counts = [0 for _ in range(len(commons))]
+        for k in group:
+            for g in range(len(counts)):
+                diff = k.a - commons[g]
+                if np.min(diff) >= 0:
+                    # sq_diff = tuple([diff[_]**2 for _ in range(len(diff))])
+                    # score = 1/(1+sum(sq_diff))
+                    # derv = tuple([0 if commons[g][_] == 0 else k[_] for _ in range(len(k))])
+                    counts[g] += np.sum(commons[g])
+        highest = counts.index(max(counts))
+        if not sampling is None:
+            highest = random.choices([i for i in range(len(counts))], weights=[w**sampling for w in counts], k=1)[0]
+        for k in group.copy():
+            diff = k.a - commons[highest]
+            if min(diff) >= 0:
+                groups[highest].add(k)
+                group.remove(k)
 
-        for g in range(len(commons)):
-            diff = tuple([(k[_]-commons[g][_]) for _ in range(len(k))])
-            if min(diff) < 0:
-                continue
-            sq_diff = tuple([diff[_]**2 for _ in range(len(diff))])
-            score = sum(sq_diff)
-            if not sampling is None:
-                sample_inds.append(g)
-                sample_weights.append(1/(.1+score)**sampling)
-            if best == None or score < best_score:
-                best = g
-                best_score = score
 
-        if best == None:
-            raise RuntimeError("Unfound common denominator: " + str(k) + " not in " + str(commons))
-        else:
-            if not sampling is None:
-                groups[random.choices(sample_inds, weights=sample_weights, k=1)[0]].add(k)
-            else:
-                groups[best].add(k)
+    # groups = [set() for _ in range(len(commons))]
+    # for k in k_list:
+    #     best = None
+    #     best_score = None
+    #     sample_inds = []
+    #     sample_weights = []
+
+    #     for g in range(len(commons)):
+    #         diff = tuple([(k[_]-commons[g][_]) for _ in range(len(k))])
+    #         if min(diff) < 0:
+    #             continue
+    #         sq_diff = tuple([diff[_]**2 for _ in range(len(diff))])
+    #         score = sum(sq_diff)
+    #         if not sampling is None:
+    #             sample_inds.append(g)
+    #             sample_weights.append(1/(.1+score)**sampling)
+    #         if best == None or score < best_score:
+    #             best = g
+    #             best_score = score
+
+    #     if best == None:
+    #         raise RuntimeError("Unfound common denominator: " + str(k) + " not in " + str(commons))
+    #     else:
+    #         if not sampling is None:
+    #             groups[random.choices(sample_inds, weights=sample_weights, k=1)[0]].add(k)
+    #         else:
+    #             groups[best].add(k)
     
-    cost = 0
     order = [i for i in range(len(groups))]
-    order.sort(reverse=True, key=lambda x: len(groups[x]))
+    order.sort(key=lambda x: len(groups[x]))
     for g in order:
 
         curr_group = groups[g]
@@ -138,39 +177,58 @@ def generalized_horners(poly, verbose:bool=False, mem=None, sampling=None, early
 
         needed = curr_d
         if len(curr_group) > 1:
-            # if verbose:
-            #     print(commons)
-            #     print(curr_group, "-->", curr_d, '\n')
+            if verbose:
+                print(commons)
+                print(curr_group, "-->", curr_d, '\n')
 
-            cost += 1
-            
-            curr_group = get_reduced(curr_group, curr_d)
+        savers = set()
+        if len(curr_group) == 1:
+            savers.add(list(curr_group)[0])
+
+        cost += 1
+        curr_group = get_reduced(curr_group, curr_d)
+        if len(curr_group) == 1:
+            savers.add(list(curr_group)[0])
+
+        curr_group, d_cost = get_clean(curr_group)
+        cost += d_cost
+        if len(curr_group) == 1:
+            savers.add(list(curr_group)[0])
+
+        elif len(curr_group) == 0:
+            continue
+
+        if len(curr_group) > 1 or list(curr_group)[0] not in mem:
             cost += generalized_horners(curr_group, verbose=verbose, mem=mem, sampling=sampling, early_stopping=(None if early_stopping is None else early_stopping-cost))
-        
         else:
-            needed = list(curr_group)[0]
+            cost += 1
 
-        if needed not in mem:
-            for trial in range(1):
-                best_diff = log_cost(needed)
-                best_tup = needed
-                for m in mem:
-                    diff = tuple([(needed[_]-m[_]) for _ in range(len(m))])
-                    if min(diff) < 0:
-                        continue
-                    if 1+log_cost(diff) < best_diff:
-                        best_diff = 1+log_cost(diff)
-                        best_tup = m
-                cost += best_diff
-                needed = tuple([needed[i] - best_tup[i] for i in range(len(needed))])
-                mem.add(needed)
-                if sum(needed) == 0:
-                    break
+        for m in savers:
+            mem.add(m)
+
+        # else:
+        #     needed = list(curr_group)[0]
+
+        # if needed not in mem:
+        #     for trial in range(1):
+        #         best_diff = log_cost(needed)
+        #         best_tup = needed
+        #         for m in mem:
+        #             diff = tuple([(needed[_]-m[_]) for _ in range(len(m))])
+        #             if min(diff) < 0:
+        #                 continue
+        #             if 1+log_cost(diff) < best_diff:
+        #                 best_diff = 1+log_cost(diff)
+        #                 best_tup = m
+        #         cost += best_diff
+        #         needed = tuple([needed[i] - best_tup[i] for i in range(len(needed))])
+        #         mem.add(needed)
+        #         if sum(needed) == 0:
+        #             break
 
         if not early_stopping is None and cost >= early_stopping:
             return early_stopping + 1
 
-    print(cost)
     return cost
 
 
@@ -180,7 +238,7 @@ def stochastic_horners(poly, iters, gamma, verbose=False):
     best_solution = non_stoch_sol
 
     for iter in range(iters):
-        sol = generalized_horners(poly, sampling=gamma, early_stopping=best_solution)
+        sol = generalized_horners(poly, sampling=gamma)
         if sol < best_solution:
             best_solution = sol
         if verbose:
@@ -343,11 +401,11 @@ def main():
     target *= target
 
     target = SparsePoly(5)
-    to_go = 1000000
-    while to_go > 0:
+    more = 1000
+    while more > 0:
         k = tuple([random.randrange(10) for i in range(5)])
         target.dict[k] = 1
-        to_go -= sum(k)
+        more -= sum(k)
     print("running...")
 
     coefs = []
@@ -356,23 +414,29 @@ def main():
         coefs.append(target[k])
         keys.append(k)
     horner = multivar_horner.HornerMultivarPolynomial(coefs, keys, rectify_input=True, keep_tree=True)
-    print("got horner...")
+    print("got horner...\n")
 
-    cost = generalized_horners(target, verbose=True)
+    cost = stochastic_horners(target, 10, 5, verbose=True)
 
     # print(target, '\n')
-    print("Multiplication Focussed:")
-    print("Multiplications:", cost)
-    print("Additions (approximate):", sum(target.dict.values()), '\n')
+    # print("Multiplication Focussed:")
+    print(" --> Cost:", cost)
+    print("")
+    # print("Additions (approximate):", sum(target.dict.values()), '\n')
 
-    print("Generalized:")
-    print("Multiplications (approximate):", cost+len(target.dict.values()))
-    print("Additions (approximate):", len(target.dict.values()), '\n')
+    # print("Generalized:")
+    # print("Multiplications (approximate):", cost+len(target.dict.values()))
+    # print("Additions (approximate):", len(target.dict.values()), '\n')
 
     print("Naive Estimate:", sum([log_cost(k) for k in target.dict.keys()]))
-    print("")
 
-    print("Horner Computation:", horner.num_ops)
+    claim = horner.num_ops
+    tree = str(horner.factorisation_tree)
+    for i in range(len(tree)):
+        if tree[i] == '^':
+            claim += np.ceil(np.log2(float(tree[i+1])))
+    print("Horner Computation:", claim)
+    #print(tree)
     print("")
 
     exit()
