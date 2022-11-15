b7f158577fa13bf92fc3677e7b7da4fc172a1046
Adam Klein <adam3207@gmail.com>
2022-11-14 17:45:39 -0800

    factorinator without worrying about coefficients

--- factorinator.py
+++ factorinator.py
@@ -18,6 +18,8 @@ def make_clean(poly: SparsePoly, key_lib=[(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0
         else:
             clean[k] = poly[k]
 
+    if len(clean) <= 1:
+        return clean, tuple([0 for _ in range(poly.n)]), outliers
 
     common_d = tuple([min([k[i] for k in clean.dict.keys()]) if len(clean.dict) > 0 else 0 for i in range(clean.n)])
 
@@ -27,106 +29,117 @@ def make_clean(poly: SparsePoly, key_lib=[(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0
         adjusted = tuple([k[i] - common_d[i] for i in range(len(common_d))])
         final[adjusted] = clean[k]
 
-    common_d = SparsePoly(poly.n, {common_d: 1})
     return final, common_d, outliers
 
 
-class Factorinator:
+def get_cost(p: SparsePoly):
 
-    def __init__(self, target: SparsePoly, costs={OPERATIONS.MULT: 1, OPERATIONS.ADD: 1}):
-        self.target = target
-        self.n = target.n
-        self.target_str = str(self.target)
-        self.max_order = [max([k[i] for k in self.target.dict.keys()]) if len(self.target) > 0 else 0 for i in range(self.n)]
+    cleaned = make_clean(p, [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)])[0]
 
-        self.op_costs = costs
+    cost = 0
 
+    for k in cleaned.dict.keys():
+        if cleaned.dict[k] != 0:
+            cost += sum(k)
+            cost += np.log(float(abs(cleaned.dict[k])))
 
-    def cost(self, p: SparsePoly):
-
-        cleaned = make_clean(p, [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)])[0]
-
-        cost = 0
-
-        for k in cleaned.dict.keys():
-            if cleaned.dict[k] != 0:
-                cost += sum(k)
-                cost += np.log(float(abs(cleaned.dict[k])))
-
-        return cost
+    return cost
 
 
-    def total_cost(self, polys):
-        cost = 0
-        for poly in polys:
-            cost += self.cost(poly)**2
-        
-        return cost
+def total_cost(polys):
+    cost = 0
+    for poly in polys:
+        cost += get_cost(poly)**2
+    
+    return cost
 
 
-    def search(self, iters):
+def search(target, iters, cum_cost):
+    dirty_target = target.copy()
 
-        ab = [None, None]
-        ab[0] = SparsePoly(self.n)
-        ab[0][[random.randint(0, 1+self.max_order[_]//2) for _ in range(self.n)]] = 1
-        ab[1] = SparsePoly(self.n)
-        ab[1][[random.randint(0, 1+self.max_order[_]//2) for _ in range(self.n)]] = 1
-        c = self.target - (ab[0]*ab[1])
+    for k in dirty_target.dict.keys():
+        if target[k] != 0:
+            target[k] = 1
 
-        best = [ab[0].copy(), ab[1].copy(), c.copy()]
-        prev_cost = self.total_cost([ab[0], ab[1], c])
-        best_cost = prev_cost
-        k = tuple([random.randint(0, 1+self.max_order[_]//2) for _ in range(self.n)])
-        momentum = 0
-        rejected = False
+    print(dirty_target)
 
-        for iter in range(iters):
-            changer = random.randrange(2)
+    target, t_d, t_out = make_clean(target)
+    if max(t_d) > 0:
+        cum_cost["cost"] += 1
 
-            delta = SparsePoly(self.n)
-            if rejected:
-                k = tuple([random.randint(0, 1+self.max_order[_]//2) for _ in range(self.n)])
+    check = make_clean(target)[0]
+    if len(check) <= 1:
+        # if sum([1 if abs(v) > 0 else 0 for v in target.dict.values()]) == 1:
+        #     cum_cost["cost"] += sum([0 if target[k]==0 else sum(k) for k in target.dict.keys()])
+        print("--- --- ---")
+        return [dirty_target]
 
-            if momentum == 0:
-                if k in ab[changer].dict.keys():
-                    momentum = random.choice([i for i in range(1, 4)] + [-i for i in range(1, 2)])
-                else:
-                    momentum = random.choice([i for i in range(1, 4)])
-            delta[k] = momentum
-             
-            if random.random() < 0.01:
-                delta -= ab[changer]
-            ab[changer] += delta
+    print("vvv vvv vvv ")
 
-            c = self.target - (ab[0]*ab[1])
-            new_cost = self.total_cost([ab[0], ab[1], c])
+    n = target.n
+    max_order = [max([k[i] for k in target.dict.keys()]) if len(target) > 0 else 0 for i in range(n)]
 
-            not_zero = sum([abs(val) for val in ab[changer].dict.values()]) > 0
+    ab = [None, None]
+    ab[0] = SparsePoly(n)
+    ab[0][[random.randint(0, 1+max_order[_]//2) for _ in range(n)]] = 1
+    ab[1] = SparsePoly(n)
+    ab[1][[random.randint(0, 1+max_order[_]//2) for _ in range(n)]] = 1
+    check = (ab[0]*ab[1])
+    for k in check.dict.keys():
+        if check[k] != 0:
+            check[k] = 1
+    c = target - check
 
-            if not_zero and new_cost < best_cost:
-                best = [ab[0].copy(), ab[1].copy(), c.copy()]
-                best_cost = new_cost
+    best = [ab[0].copy(), ab[1].copy(), c.copy()]
+    prev_cost = total_cost([ab[0], ab[1], c])
+    best_cost = prev_cost
+    k = tuple([random.randint(0, 1+max_order[_]//2) for _ in range(n)])
+    momentum = 0
+    rejected = False
 
-            # print("\nd:", delta)
-            # print("\na:", ab[0])
-            # print("\nb:", ab[1])
-            # print("\nc:", c)
-            # input()
-            # print(new_cost, prev_cost)
-            if new_cost > prev_cost:
-                rejected = True
-                momentum = 0
-            else:
-                rejected = False
+    for iter in range(iters):
+        changer = random.randrange(2)
 
-            if not_zero and (new_cost < prev_cost or random.random() < 0.01):
-                prev_cost = new_cost
-            else:
-                ab[changer] -= delta
+        delta = SparsePoly(n)
+        k = tuple([random.randint(0, 1+max_order[_]//2) for _ in range(n)])
+        delta[k] = random.choice([-1, 1])
+            
+        if random.random() < 0.01:
+            delta -= ab[changer]
+        ab[changer] += delta
 
-        # print("Target Cost:", self.cost(self.target))
-        # print("Solution Cost:", np.sqrt(best_cost))
-        return best
+        check = (ab[0]*ab[1])
+        for k in check.dict.keys():
+            if check[k] != 0:
+                check[k] = 1
+        c = target - check
+
+        new_cost = total_cost([ab[0], ab[1], c])
+
+        not_zero = sum([abs(val) for val in ab[changer].dict.values()]) > 0
+
+        if not_zero and new_cost < best_cost:
+            best = [ab[0].copy(), ab[1].copy(), c.copy()]
+            best_cost = new_cost
+
+        if new_cost > prev_cost:
+            rejected = True
+            momentum = 0
+        else:
+            rejected = False
+
+        if not_zero and (new_cost < prev_cost or random.random() < 0.01):
+            prev_cost = new_cost
+        else:
+            ab[changer] -= delta
+
+    # print([str(b) for b in best])
+    cum_cost["cost"] += 1
+    return (
+        search(best[0], iters, cum_cost) +
+        search(best[1], iters, cum_cost) +
+        search(best[2], iters, cum_cost)
+    )
 
 
 def main():
@@ -143,42 +156,11 @@ def main():
     # target[3, 3, 2] = 7
     # target *= target
     
-    # print("\nTarget:", target)
-    # clean, common_d, outliers = make_clean(target, [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)])
-    # print("\nClean:", clean)
-    # print("\nCommon D:", common_d)
-    # print("\nOutliers:", outliers)
-    # exit()
-
-    engine = Factorinator(make_clean(target)[0])
-    sol = engine.search(10000)
-
-    for der in sol:
-        print("")
-        print(der, "--->", make_clean(der)[0])
-    print("-----------------------------")
-
-    sol_cost = 0
-    for s in sol:
-        sengine = Factorinator(make_clean(s)[0])
-        der = sengine.search(10000)
-        for d in der:
-            print("")
-            print(d, "--->", make_clean(d)[0])
-            sol_cost += sengine.cost(d)**2
-    
-    print("")
-    print("Initial Cost:", engine.cost(target))
-    print("Intermediate Cost:", sum([engine.cost(s)**2 for s in sol])**0.5)
-    print("Solution Cost:", sol_cost**0.5)
-    print(target, "--->", make_clean(target)[0])
-    exit()
-
-    print("\nTarget:", target)
-    print("\na:", sol[0])
-    print("\nb:", sol[1])
-    print("\nc:", sol[2])
-    print("")
+    cum_cost = {"cost": 0}
+    solutions = search(target, 1000, cum_cost)
+    print("COST:", cum_cost["cost"])
+    # for sol in solutions:
+    #     print(sol)
 
 if __name__ == '__main__':
     main()
